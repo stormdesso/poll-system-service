b74d1847fcf40b1ce751154458c232bb665a754f
stormdesso <stormdesso@gmail.com>
2024-05-28 18:02:26 +0500

    fixed: download files

--- src/main/java/ru/pstu/poll_system_service/data/service/impl/FileServiceImpl.java
+++ src/main/java/ru/pstu/poll_system_service/data/service/impl/FileServiceImpl.java
@@ -10,7 +10,6 @@ import org.springframework.web.multipart.MultipartFile;
 import ru.pstu.poll_system_service.data.mapper.FileMapper;
 import ru.pstu.poll_system_service.data.model.FileEntity;
 import ru.pstu.poll_system_service.data.repository.FileDbRepository;
-import ru.pstu.poll_system_service.data.repository.PollRepository;
 import ru.pstu.poll_system_service.data.service.FileService;
 import ru.pstu.poll_system_service.data.service.GeneralService;
 import ru.pstu.poll_system_service.web.dto.FileDto;
@@ -25,28 +24,22 @@ import java.util.stream.Collectors;
 @Service
 @Slf4j
 @RequiredArgsConstructor
-public class FileServiceImpl implements FileService{
+public class FileServiceImpl implements FileService {
 
     private final FileDbRepository fileDbRepository;
-    private final PollRepository pollRepository;
     private final GeneralService generalService;
 
     @Override
-    public List<FileInfoDto> getFilesInfosByPollId(Long pollId){
+    public List<FileInfoDto> getFilesInfosByPollId(Long pollId) {
         generalService.hasAccessToPoll(pollId);
         return FileMapper.INSTANCE.toFileInfoDtos(fileDbRepository.findByPollId(pollId));
     }
 
     @Override
-    public List<FileDto> getFilesListByPollId(Long pollId,List<Long> requestedFilesIds){
+    public List<FileDto> getFilesListByPollId(Long pollId, List<Long> requestedFilesIds) {
         var availableFilesIds = fileDbRepository.getFilesIdsByPollId(pollId);
-        var resultSet = new HashSet<Long>(availableFilesIds);
+        var resultSet = new HashSet<>(availableFilesIds);
         resultSet.retainAll(requestedFilesIds);
-//        availableFilesIds.forEach( fileId -> {
-//            if (requestedFilesIds.contains(fileId)){
-//                resultSet.add(fileId);
-//            }
-//        });
         //скачиваем все файлы из списка, которые есть в запрашиваемом списке
         var files = fileDbRepository.findByIdIn(resultSet);
         return FileMapper.INSTANCE.toFileDtos(files);
@@ -55,25 +48,17 @@ public class FileServiceImpl implements FileService{
     @Override
     @Transactional
     public List<FileInfoDto> save(List<MultipartFile> files, Long pollId) {
+        generalService.hasAccessToPoll(pollId);
         List<FileEntity> savedFiles = new ArrayList<>();
 
         files.forEach(file -> {
             try {
-                String originalName = file.getOriginalFilename();
-                byte[] data = file.getBytes();
-                String type = file.getContentType() == null ? "unknown" : file.getContentType();
-                String size = FileUtils.byteCountToDisplaySize(file.getSize());
-
-                // Логирование данных перед созданием FileEntity
-                log.info("Saving file: originalName={}, type={}, size={}", originalName, type, size);
-
-
+                var extension = FilenameUtils.getExtension((file.getOriginalFilename()));
                 FileEntity fileEntity = FileEntity.builder()
                         .pollId(pollId)
-                        .originalName(file.getOriginalFilename()) // надо убрать расширение
+                        .originalName(file.getOriginalFilename())
                         .data(file.getBytes())
-                        .type(FilenameUtils.getExtension(file.getOriginalFilename() == null? "unknown":
-                                FilenameUtils.getExtension(file.getOriginalFilename())))
+                        .type(extension == null || extension.isEmpty() ? "unknown" : extension)
                         .size(FileUtils.byteCountToDisplaySize(file.getSize()))
                         .build();
                 savedFiles.add(fileDbRepository.save(fileEntity));
@@ -87,7 +72,7 @@ public class FileServiceImpl implements FileService{
 
     @Override
     @Transactional
-    public void delete (List<Long> filesIds){
+    public void delete(List<Long> filesIds) {
         var files = fileDbRepository.findFileEntitiesByIdIsIn(filesIds);
         generalService.hasAccessToPolls(files.stream().map(FileEntity::getPollId).collect(Collectors.toList()));
         fileDbRepository.deleteAllById(filesIds);
--- src/main/java/ru/pstu/poll_system_service/web/controller/FileController.java
+++ src/main/java/ru/pstu/poll_system_service/web/controller/FileController.java
@@ -4,9 +4,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import ru.pstu.poll_system_service.business.aspect.HasPermission;
@@ -17,6 +15,9 @@ import ru.pstu.poll_system_service.web.dto.FileInfoDto;
 import java.io.IOException;
 import java.util.List;
 
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
 import static ru.pstu.poll_system_service.web.security.constant.ActionConstants.*;
 import static ru.pstu.poll_system_service.web.security.constant.SystemObjectConstants.POLL;
@@ -25,7 +26,7 @@ import static ru.pstu.poll_system_service.web.security.constant.SystemObjectCons
 @RequestMapping("/api/v1/file")
 @RequiredArgsConstructor
 @Slf4j
-public class FileController{
+public class FileController {
 
     private final FileService fileService;
 
@@ -35,22 +36,20 @@ public class FileController{
     @GetMapping
     public List<FileInfoDto> getFilesInfoByPollId(
             @Parameter(description = "Идентификатор опроса")
-            @RequestParam(required = true) Long pollId
-    ){
+            @RequestParam(required = true) Long pollId) {
         return fileService.getFilesInfosByPollId(pollId);
     }
 
     @Operation(description = "Скачать файлы, связанные с опросом")
     @HasPermission(resource = POLL, action = READ)
     @ResponseBody
-    @PostMapping("/download_list")
-    public List<FileDto>  downloadFilesList(
+    @PostMapping(value = "/download_list", produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<FileDto>> downloadFilesList(
             @Parameter(description = "Идентификатор опроса")
             @RequestParam(required = true) Long pollId,
             @Parameter(description = "Список идентификаторов файлов")
-            @RequestBody(required = true) List<Long> ids
-    ){
-        return fileService.getFilesListByPollId(pollId, ids);
+            @RequestParam(required = true) List<Long> ids) {
+        return new ResponseEntity<>(fileService.getFilesListByPollId(pollId, ids), OK);
     }
 
     @Operation(description = "Загрузить файлы на сервер")
@@ -58,26 +57,23 @@ public class FileController{
     @ResponseBody
     @PostMapping(value = "/upload", consumes = MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<List<FileInfoDto>> upload(
-            //@RequestParam(required = true)
             @RequestParam("files") MultipartFile[] files,
             @RequestParam(required = true) Long pollId) {
         try {
             var savedFilesInfos = fileService.save(List.of(files), pollId);
-            return new ResponseEntity<>(savedFilesInfos, HttpStatus.OK);
+            return new ResponseEntity<>(savedFilesInfos, OK);
         } catch (IOException e) {
             log.error("Ошибка при попытке сохранить файл :{}", e.getMessage());
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(INTERNAL_SERVER_ERROR);
         }
-
     }
+
     @Operation(description = "Удалить файлы по id")
     @HasPermission(resource = POLL, action = DELETE)
     @ResponseBody
     @DeleteMapping(value = "/delete")
-    public void delete(
-            @Parameter(description = "Идентификаторы файлов")
-            @RequestParam(required = true) List<Long> ids
-    ){
+    public void delete(@Parameter(description = "Идентификаторы файлов")
+                           @RequestParam(required = true) List<Long> ids) {
         fileService.delete(ids);
     }
 
