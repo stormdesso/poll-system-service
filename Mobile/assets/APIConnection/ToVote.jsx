ec2fd9575cfb36a19d52ad20e8230c40f590f6b6
dneis2489 <denis-pelenev@inbox.ru>
2024-06-03 13:22:55 +0500

    Подключил API для голосования

--- Mobile/assets/APIConnection/ToVote.jsx
+++ Mobile/assets/APIConnection/ToVote.jsx
@@ -3,21 +3,29 @@ import * as SecureStore from 'expo-secure-store';
 import { SearchProp } from '../Data/SearchProp';
 
 export default function ToVote(selectValue, pollValues, pollId) {
-    console.log(selectValue)
-    console.log(pollValues)
+    let requestBody = [];
 
+    const findObjectById = (id) => {
+        return pollValues.find(item => item.id === id)
+    };
+
+    for (const item of selectValue){
+        let poll = findObjectById(item)
+        poll.votes += 1
+        requestBody.push(poll)
+    }    
     return new Promise((resolve, reject) => {
         
         SecureStore.getItemAsync('Token')
             .then(token => {
-                let url = `http://192.168.0.159:8080/api/v1/poll/vote`;
+                let url = `http://192.168.0.159:8080/api/v1/poll/vote?pollId=${pollId}`;
                 let options = {
                     method: "POST",
                     headers: {
-                        Connection: "keep-alive",
+                        "Connection": "keep-alive",
                         "Content-Type": "application/json",
-                        accept: "*/*",
-                        authorization: token,
+                        "Accept": "*/*",
+                        "authorization": token
                     },
                     body: JSON.stringify(requestBody),
                 };
@@ -25,15 +33,15 @@ export default function ToVote(selectValue, pollValues, pollId) {
                 fetch(url, options)
                     .then((response) => {
                         if (!response.ok) {
-                            throw new Error("Failed to fetch poll list");
+                            throw new Error("Failed to fetch vote");
                         }
                         return response.json();
                     })
-                    .then((data) => {
-                        resolve(data);
+                    .then(() => {
+                        resolve("Вы успешно проголосовали");
                     })
                     .catch((error) => {
-                        console.error("Error fetching poll list:", error);
+                        console.error("Error fetching vote:", error);
                         reject(error);
                     });
             })
--- Mobile/assets/elements/specialElements/ToVoteBlock.jsx
+++ Mobile/assets/elements/specialElements/ToVoteBlock.jsx
@@ -5,7 +5,6 @@ import ToVote from "../../APIConnection/ToVote"
 
 export const ToVoteBlock = ({pollValues, maxAnswers, pollId}) => {
     const [selectedOptions, setSelectedOptions] = useState([]);
-    pollId
     //Определяет логику отметки выбранных элементов
     const handleSelection = (id) => {
         if (maxAnswers === 1) {
@@ -24,7 +23,6 @@ export const ToVoteBlock = ({pollValues, maxAnswers, pollId}) => {
     const handleVote = () => {
         console.log('Selected option IDs:', selectedOptions);
         ToVote(selectedOptions, pollValues, pollId)
-        // Здесь можно добавить логику отправки данных на сервер или другого дальнейшего использования
     };
 
     
