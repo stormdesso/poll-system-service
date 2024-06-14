964c9c8c6f15447d70f7c41503d2d97ec1c6fc37
stormdesso <stormdesso@gmail.com>
2024-06-14 15:58:00 +0500

    fixed: encode password

--- src/main/java/ru/pstu/poll_system_service/data/service/impl/UserServiceImpl.java
+++ src/main/java/ru/pstu/poll_system_service/data/service/impl/UserServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import ru.pstu.poll_system_service.data.enums.RoleEnum;
 import ru.pstu.poll_system_service.data.mapper.UserMapper;
@@ -54,6 +55,8 @@ public class UserServiceImpl implements UserService{
     private final OwnershipAddressRepository ownershipAddressRepository;
 
     private final OwnershipRepository ownershipRepository;
+
+    private final PasswordEncoder passwordEncoder;
 
 
 
@@ -154,7 +157,7 @@ public class UserServiceImpl implements UserService{
                 .phoneNumber(userDto.getPhoneNumber())
                 .birthdate(userDto.getBirthdate())
                 .email(userDto.getEmail())
-                .password( password.isBlank() ? userEntity.getPassword(): password)
+                .password( password.isBlank() ? userEntity.getPassword(): passwordEncoder.encode(password))
                 .build();
 
         userWithAddressRepository.save(changedUserEntity);
